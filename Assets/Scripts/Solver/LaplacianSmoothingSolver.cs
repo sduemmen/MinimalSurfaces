df0a87d7d9aa9f457eeb513b6b33d8455b93a6ba
sduemmen <sduemmen@students.uni-mainz.de>
2026-03-28 22:40:57 +0100

    refactored LaplacianSmoothingSolver to use matrix-based computations and updated solver assets accordingly

--- Assets/Scripts/Solver/LaplacianSmoothingSolver.cs
+++ Assets/Scripts/Solver/LaplacianSmoothingSolver.cs
@@ -1,60 +1,82 @@
 ﻿using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
 using Meshes;
 using UnityEngine;
 
 namespace Solver {
-    [CreateAssetMenu(menuName = "Minimal Surfaces/Solvers/Laplacian Smoothing")]
+    [CreateAssetMenu(menuName = "Minimal Surfaces/Solvers/Laplacian Smoothing Matrix")]
     public class LaplacianSmoothingSolver : MinimalSurfaceSolverBase {
         public float dt = 1f;
         public float convergenceTolerance = 1e-05f;
-
-        Vector3[] _displacement;
-        bool _initialized;
+        
+        Matrix<float> L;
+        
+        Vector<float> xs;
+        Vector<float> ys;
+        Vector<float> zs;
+        Vector<float> dx;
+        Vector<float> dy;
+        Vector<float> dz;
 
 
         public override void Initialize(MeshData meshData, MinimalSurface context) {
-            _displacement = new Vector3[meshData.vertices.Length];
-            _initialized = true;
+            BuildLaplaceMatrix(meshData);
+            
+            int n = meshData.vertices.Length;
+            xs = Vector<float>.Build.Dense(n);
+            ys = Vector<float>.Build.Dense(n);
+            zs = Vector<float>.Build.Dense(n);
+            dx = Vector<float>.Build.Dense(n);
+            dy = Vector<float>.Build.Dense(n);
+            dz = Vector<float>.Build.Dense(n);
         }
 
         public override bool Step(MeshData meshData, MinimalSurface context) {
-            if (!_initialized) {
-                Debug.LogError("Solver has not been initialized.");
-                return true;
+            int n = meshData.vertices.Length;
+            for (int i = 0; i < n; i++) {
+                Vector3 v = meshData.vertices[i];
+                xs[i] = v.x;
+                ys[i] = v.y;
+                zs[i] = v.z;
             }
-
-            for (int p = 0; p < meshData.vertices.Length; p++) {
-                _displacement[p] = Vector3.zero;
-                List<(int, int)> adjacentVertexPairs = meshData.triangleNeighborPairsByVertex[p];
-
-                if (meshData.fixedVertices[p] || adjacentVertexPairs.Count == 0) continue;
-
-                Vector3 c = Vector3.zero;
-                foreach ((int a, int b) in adjacentVertexPairs) {
-                    c += meshData.vertices[a] + meshData.vertices[b];
-                }
-
-                c /= 2f * adjacentVertexPairs.Count;
-                _displacement[p] = c - meshData.vertices[p];
-            }
+            
+            L.Multiply(xs, dx);
+            L.Multiply(ys, dy);
+            L.Multiply(zs, dz);
 
             float maxGrad = 0f;
+            for (int i = 0; i < n; i++) {
+                if (meshData.fixedVertices[i]) continue;
 
-            for (int p = 0; p < meshData.vertices.Length; p++) {
-                if (meshData.fixedVertices[p]) continue;
+                Vector3 displacement = new Vector3(dx[i], dy[i], dz[i]);
 
-                float d = _displacement[p].magnitude;
+                float d = Mathf.Sqrt(Vector3.Dot(displacement, displacement));
                 if (d > maxGrad) {
                     maxGrad = d;
                 }
 
-                meshData.vertices[p] += dt * _displacement[p];
+                meshData.vertices[i] += dt * displacement;
             }
 
             meshData.mesh.SetVertices(meshData.vertices);
             meshData.mesh.RecalculateNormals();
-
+            
             return maxGrad < convergenceTolerance;
+        }
+
+        void BuildLaplaceMatrix(MeshData meshData) {
+            int n = meshData.vertices.Length;
+            L = Matrix<float>.Build.Sparse(n, n);
+            
+            for (int i = 0; i < meshData.vertices.Length; i++) {
+                HashSet<int> neighbors = meshData.neighbors[i];
+                if (meshData.fixedVertices[i] || neighbors.Count == 0) continue;
+                
+                L[i, i] = -1f;
+                foreach (int j in neighbors) {
+                    L[i, j] = 1f / neighbors.Count;
+                }
+            }
         }
     }
 }
